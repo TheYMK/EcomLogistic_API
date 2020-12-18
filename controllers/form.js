5a8556df3fa4e35b25981bed5e0e6d992d7ce5ec
Kaym Kassai <kaymkassai269@gmail.com>
2020-12-18 21:42:41 +0300

    Add contact form routes and form validators

---
+++ controllers/form.js
@@ -0,0 +1,24 @@
+const { sendEmailWithNodemailer } = require('../helpers/email');
+
+exports.contactForm = (req, res) => {
+	console.log(req.body);
+	const { name, email, message } = req.body;
+
+	const emailData = {
+		from: process.env.EMAIL, // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+		to: 'contact@ecomores-services.com', // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE YOUR GMAIL
+		subject: `${process.env.APP_NAME} | Contact Form`,
+		text: `Un email vous a été envoyé depuis le site E-Com logistics \n Nom de l'envoyeur: ${name} \n Email de l'envoyeur: ${email} \n Message de l'envoyeur: ${message}`,
+		html: `
+        <h4>Email reçu depuis la section contact du site E-Com logistics</h4>
+        <p>Nom de l'envoyeur: ${name}</p>
+        <p>Email de l'envoyeur: ${email}</p>
+        <p>Message de l'envoyeur: ${message}</p>
+        <hr />
+        <p>Cet email contient des informations sensitive. À ne pas partager.</p>
+		<p>https://ecomores-services.com</p>
+    `
+	};
+
+	sendEmailWithNodemailer(req, res, emailData);
+};
